02bba5e7a0799a72a4cb1c4fa3c7970e2257bae2
abishek2008 <69070114+abishek2008@users.noreply.github.com>
2021-12-22 15:36:29 +0530

    Add files via upload

---
+++ c107_project.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import csv
+
+df=pd.read_csv('studentlevel_data.csv')
+mean=df.groupby(["student_id","level"],as_index=False)["attempt"].mean()
+
+fig=px.scatter(
+    mean,x="student_id",
+    y="level",size="attempt",color="attempt"
+)
+fig.show()
